fb12d2b85fac8bf40e5f374c746a3ef13277651e
VitaliyHraboviak <117772319+VitaliyHraboviak@users.noreply.github.com>
2023-03-21 20:36:22 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,13 +3,12 @@ const refs = {
 };
 
 refs.input.addEventListener('blur', onInputChange);
-
+const dataLength = refs.input.dataset.length;
 let valueLength = 0;
-   
 function onInputChange(event) {
     valueLength = event.currentTarget.value.length; 
     console.log(event.currentTarget.value.length);
-    if (valueLength < 6 || valueLength > 6) {
+    if (valueLength < dataLength || valueLength > dataLength) {
         refs.input.classList.add("invalid");
         
     } else { refs.input.classList.replace("invalid", "valid") };
